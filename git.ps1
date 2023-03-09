d4c5b4ed448a09dbaa0aff4ca7535e2c70379e12
Rohan Kaushik <rohankaushik1@outlook.com>
2023-03-09 21:43:29 +0530

    first push

---
+++ git.ps1
@@ -0,0 +1,21 @@
+# auto setup local git repo
+
+if (!(Test-Path -Path ".git")) {
+    git init
+
+    $remote = Read-Host "Enter remote URL: "
+
+    git remote add origin $remote
+}
+
+Write-Output "Waiting for setup"
+
+Start-Sleep -Seconds 2
+
+git pull
+
+git add .
+$message = Read-Host "Enter commit message: "
+git commit -S -m "$message" 
+
+git push -u origin master
