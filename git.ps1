8d4f73d17d38a37a2acbf092db09391c72ac7b67
Rohan Kaushik <rohankaushik1@outlook.com>
2023-03-09 22:01:50 +0530

    2 new scripts

--- git.ps1
+++ git.ps1
@@ -1,6 +1,6 @@
 # auto setup local git repo
 
-if (!(Test-Path -Path ".git")) {
+if(!(Test-Path -Path ".git")){
     git init
 }
 
@@ -9,7 +9,7 @@ $message = Read-Host "Enter commit message: "
 git commit -S -m "$message"
 git branch -M master
 
-if (!(Test-Path -Path ".git")) {
+if(!(Test-Path -Path ".git")){
     $remote = Read-Host "Enter remote URL: "
     git remote add origin $remote
 }
@@ -17,5 +17,5 @@ if (!(Test-Path -Path ".git")) {
 Write-Output "Waiting for setup"
 
 Start-Sleep -Seconds 2
- 
+
 git push -u origin master
